7a5647ccaebf2379a5455ba0fcf2fa9b9cf4bfa1
vbarobotic <cgreenlee@vbainfo.com>
2019-02-15 14:26:49 -0600

    Create relay.py

---
+++ relay.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO,time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+redled = 23
+fountain = 25
+GPIO.setup(redled,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(fountain,GPIO.OUT,initial=GPIO.HIGH)
+GPIO.output(redled,GPIO.HIGH)
+time.sleep(5)
+GPIO.output(redled,GPIO.LOW)
+GPIO.output(fountain,GPIO.LOW)
+time.sleep(10)
+GPIO.output(fountain,GPIO.HIGH)
